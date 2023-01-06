43df7c101f9234ea7e51a506fbed0f24120d1b66
bodwood <bodiewood@hotmail.com>
2023-01-06 15:05:13 -0800

    add relationship to mechanic

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,5 +7,7 @@ namespace Factory.Models
   {
     public int MachineId { get; set; }
     public string Name { get; set; }
+    public int MechanicId { get; set; }
+    public Mechanic Mechanic { get; set; }
   }
 }
