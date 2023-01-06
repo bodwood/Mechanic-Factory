4b3ac8fd1b4e897ab218e9d275785dafe2403a12
bodwood <bodiewood@hotmail.com>
2023-01-06 13:05:48 -0800

    add edit view

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Factory.Models{
-
+namespace Factory.Models
+{
   public class Machine
   {
     public int MachineId { get; set; }
