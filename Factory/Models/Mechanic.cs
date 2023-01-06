6f895067bd54a873327f5baf09abb4548e7c3d75
bodwood <bodiewood@hotmail.com>
2023-01-06 11:24:46 -0800

    add model of mechanic to represent mechanic table in database

---
+++ Factory/Models/Mechanic.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class Mechanic
+  {
+    public int MechanicId { get; set; }
+    public string Name { get; set; }
+  }
+}
