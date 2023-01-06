da6b222d5e28792bbc079d51e70a13d5ef5ef940
bodwood <bodiewood@hotmail.com>
2023-01-06 12:13:40 -0800

    add database connection  to machinescontroller.cs

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Factory.Models;
+using System.Linq;
+
+namespace MachinesController.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+  }
+}
