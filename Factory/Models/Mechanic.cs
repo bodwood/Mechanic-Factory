6508899a8f82096e547a29f1622c1561387d7eec
bodwood <bodiewood@hotmail.com>
2023-01-06 18:37:03 -0800

    add error message if a name for the mechanic is not entered

--- Factory/Models/Mechanic.cs
+++ Factory/Models/Mechanic.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
   public class Mechanic
   {
     public int MechanicId { get; set; }
+    [Required(ErrorMessage = "Employee name must be entered")]
     public string Name { get; set; }
-    public List<Machine> Machines { get; set; }
+     public List<MechanicMachine> JoinEntities { get; }
   }
 }
