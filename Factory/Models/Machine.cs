fd5cbcbe7dbac68b7c42a444f942143578cef4e5
bodwood <bodiewood@hotmail.com>
2023-01-06 18:38:14 -0800

    add error message in Machine does not have a name when created

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "Machine name must be entered")]
     public string Name { get; set; }
-    public int MechanicId { get; set; }
-    public Mechanic Mechanic { get; set; }
+    public List<MechanicMachine> JoinEntities { get; }
   }
 }
