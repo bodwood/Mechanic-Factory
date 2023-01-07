c378d3948246b6ed2341c2e755bf9e46f2f1156a
bodwood <bodiewood@hotmail.com>
2023-01-06 18:43:01 -0800

    add database connection to home controller

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+    
       return View();
     }
   }
