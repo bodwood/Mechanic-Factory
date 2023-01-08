21f5dc69476ac8ca942c3d8d14b0226b4e336a3a
bodwood <bodiewood@hotmail.com>
2023-01-07 16:07:33 -0800

    insert mechanidId int that was accidentally removed

--- Factory/Models/MechanicMachine.cs
+++ Factory/Models/MechanicMachine.cs
@@ -3,6 +3,7 @@ namespace Factory.Models{
   {
     public int MechanicMachineId { get; set; }
     public int MachineId { get; set; }
+    public int MechanicId { get; set; }
     public Machine Machine { get; set; }
     public Mechanic Mechanic { get; set; }
   }
